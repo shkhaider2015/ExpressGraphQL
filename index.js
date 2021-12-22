70b5dbd6edbe663465098e4b8438520a63ec4239
Shakeel Haider <shkhaider2015@gmail.com>
2021-12-23 01:22:09 +0500

    initialize project

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express/');
+// const routes = require("./routes/routes");
+// const {connect} = require('./config/database');
+const app = express();
+const port = 8000;
+
+// establish connection with database
+// connect();
+
+
+// use external routes file
+// app.use("/", routes);
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
